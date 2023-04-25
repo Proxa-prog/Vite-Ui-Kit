acf29b61a2f0a922dce6f15bd81b7ad2d275b5a5
iprohorov <iprohorov@mmtr.ru>
2023-04-25 16:52:27 +0300

    0.3.1

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
             <Button onClick={handleClick} color='red'>Viteeeeee</Button>
             <img src={fish} width={50} height={50}/>
             <img src={fish} width={50} height={50}/>
+            <img src={fish} width={50} height={50}/>
         </>
     )
 }
